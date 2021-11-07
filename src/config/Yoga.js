f03e5f325493cd8cff2751914cf7c98bbe289d23
Adam Yen <adamyen@Adams-MacBook-Pro.local>
2021-11-07 18:32:48 -0500

    [feat]: yoga classes configuration isolated

---
+++ src/config/Yoga.js
@@ -0,0 +1,34 @@
+const YogaClasses = [
+    {
+        src: '/images/R11.jpg',
+        title: 'Yoga for Beginners',
+        description: 'A collection of dumbbell workouts by your favorite trainers specific to particular muscle group.',
+        locationState: {
+            videoId: 'v7AYKMP6rOE',
+            title: 'Yoga for beginners',
+            description: "Yoga is a vast collection of spiritual techniques and practices aimed at integrating mind, body, and spirit to achieve a state of enlightenment or oneness with the universe. What is normally thought of as “yoga” in the West is really Hatha Yoga, one of the many paths of yoga. The different paths of yoga emphasize different approaches and techniques but ultimately lead to the same goal of unification and enlightenment. Hatha Yoga attains the union of mind-body-spirit through a practice of asanas (yoga postures), pranayama (yoga breathing), mudra (body gestures), and shatkarma (internal cleansing). These physical practices are used to purify the body and cultivate prana (life-force energy). Modern Hatha Yoga does not emphasize many of these esoteric practices and instead focuses more on the physical yoga postures. Regardless of what your goals or intentions are for starting, just the yoga poses themselves is a fantastic form of mental and physical exercise."
+        },
+    },
+    {
+        src: '/images/R12.jpg',
+        title: 'Yoga for Intermediate',
+        description: 'A collection of dumbbell workouts by your favorite trainers specific to particular muscle group.',
+        locationState: {
+            videoId: '_LvGTQ3Aq-g',
+            title: 'Yoga for Intermediate',
+            description: "Yoga is a vast collection of spiritual techniques and practices aimed at integrating mind, body, and spirit to achieve a state of enlightenment or oneness with the universe. What is normally thought of as “yoga” in the West is really Hatha Yoga, one of the many paths of yoga. The different paths of yoga emphasize different approaches and techniques but ultimately lead to the same goal of unification and enlightenment. Hatha Yoga attains the union of mind-body-spirit through a practice of asanas (yoga postures), pranayama (yoga breathing), mudra (body gestures), and shatkarma (internal cleansing). These physical practices are used to purify the body and cultivate prana (life-force energy). Modern Hatha Yoga does not emphasize many of these esoteric practices and instead focuses more on the physical yoga postures. Regardless of what your goals or intentions are for starting, just the yoga poses themselves is a fantastic form of mental and physical exercise."
+        },
+    },
+    {
+        src: '/images/R13.jpg',
+        title: 'Yoga for Advanced',
+        description: 'A collection of dumbbell workouts by your favorite trainers specific to particular muscle group.',
+        locationState: {
+            videoId: 'CiaD3jP0YhA',
+            title: 'Yoga for Advanced',
+            description: "Yoga is a vast collection of spiritual techniques and practices aimed at integrating mind, body, and spirit to achieve a state of enlightenment or oneness with the universe. What is normally thought of as “yoga” in the West is really Hatha Yoga, one of the many paths of yoga. The different paths of yoga emphasize different approaches and techniques but ultimately lead to the same goal of unification and enlightenment. Hatha Yoga attains the union of mind-body-spirit through a practice of asanas (yoga postures), pranayama (yoga breathing), mudra (body gestures), and shatkarma (internal cleansing). These physical practices are used to purify the body and cultivate prana (life-force energy). Modern Hatha Yoga does not emphasize many of these esoteric practices and instead focuses more on the physical yoga postures. Regardless of what your goals or intentions are for starting, just the yoga poses themselves is a fantastic form of mental and physical exercise."
+        },
+    }
+];
+
+export default YogaClasses;
--- src/views/workout/Workout.js
+++ src/views/workout/Workout.js
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from 'prop-types';
 import YouTube from 'react-youtube';
 import './Workout.css';
 
@@ -18,31 +17,22 @@ function Workout(props) {
         event.target.pauseVideo();
     }
 
+    const { videoId, title, description } = props.location.state;
     return (
-        <div class="wrapper">
-            <div class="container">
+        <div className="wrapper">
+            <div className="container">
                 <YouTube
-                    videoId={props.videoId || 'v7AYKMP6rOE'}
+                    videoId={videoId || 'v7AYKMP6rOE'}
                     opts={opts}
                     onReady={_onReady}
                 />
             </div>
-            <div class="container-col">
-                <h2>{props.title}</h2>
-                <h4>{props.description}</h4>
+            <div className="container-col">
+                <h2>{title}</h2>
+                <h4>{description}</h4>
             </div>
         </div>
     );
 }
 
 export default Workout;
-
-Workout.propTypes = {
-    title: PropTypes.string,
-    description: PropTypes.string,
-}
-
-Workout.defaultProps = {
-    title: 'Yoga for beginners',
-    description: "Yoga is a vast collection of spiritual techniques and practices aimed at integrating mind, body, and spirit to achieve a state of enlightenment or oneness with the universe. What is normally thought of as “yoga” in the West is really Hatha Yoga, one of the many paths of yoga. The different paths of yoga emphasize different approaches and techniques but ultimately lead to the same goal of unification and enlightenment. Hatha Yoga attains the union of mind-body-spirit through a practice of asanas (yoga postures), pranayama (yoga breathing), mudra (body gestures), and shatkarma (internal cleansing). These physical practices are used to purify the body and cultivate prana (life-force energy). Modern Hatha Yoga does not emphasize many of these esoteric practices and instead focuses more on the physical yoga postures. Regardless of what your goals or intentions are for starting, just the yoga poses themselves is a fantastic form of mental and physical exercise."
-}
