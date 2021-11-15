6191613a6c26c6de35d2f31ddf21788f79a44b08
Adam Yen <adamyen@Adams-MacBook-Pro.local>
2021-11-14 19:07:46 -0500

    [feat]: Card proptypes and icon

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
 import './Card.css';
 
 function Card(props){
-    console.log('>>>> Card props', props)
     let history = useHistory();
 
     function handleClick() {
@@ -26,11 +27,27 @@ function Card(props){
         >
             <img src={props.src} alt="" />
             <div className="card__info">
-                <h2>{props.title}</h2>
+                <div className="flex-row">
+                    <h3>{props.title}</h3>
+                    {props.check && (<CheckCircleIcon />)}
+                </div>
                 {props.description && (<h4>{props.description}</h4>)}
             </div>
         </div>
     )
+}
+
+Card.propTypes = {
+    disableClick: PropTypes.bool,
+    onClick: PropTypes.func,
+    url: PropTypes.string,
+    src: PropTypes.string,
+    locationState: PropTypes.shape({}),
+    width: PropTypes.number,
+    height: PropTypes.number,
+    title: PropTypes.string,
+    check: PropTypes.bool,
+    description: PropTypes.string,
 }
 
 export default Card;
