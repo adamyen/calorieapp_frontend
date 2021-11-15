942d75002e45f365cbfae84612a17c99eaa3f5e9
Adam Yen <adamyen@Adams-MacBook-Pro.local>
2021-11-14 19:08:15 -0500

    [feat]: add calories

--- src/config/Workout.js
+++ src/config/Workout.js
@@ -5,7 +5,8 @@ const Workouts = [
         locationState: {
             videoId: 'nmNCH-Ueq8E',
             title: 'Walk Fitness',
-            description: "Walking is a great way to improve or maintain your overall health.\nTry this 20 min indoor fat burning workout with growwithjo to start burning some fat!"
+            description: "Walking is a great way to improve or maintain your overall health.\nTry this 20 min indoor fat burning workout with growwithjo to start burning some fat!",
+            calories: 660
         },
     },
     {
@@ -14,7 +15,8 @@ const Workouts = [
         locationState: {
             videoId: 'Cw-Wt4xKD2s',
             title: 'Dance Fitness',
-            description: "If you are looking for something more intense, Dance Fitness is a great choice!\nDance fitness is a type of exercise class that incorporates some or many forms of dance.\nTry this 15 min Dance Fitness Workout with Pamela to start burning some fat!"
+            description: "If you are looking for something more intense, Dance Fitness is a great choice!\nDance fitness is a type of exercise class that incorporates some or many forms of dance.\nTry this 15 min Dance Fitness Workout with Pamela to start burning some fat!",
+            calories: 720
         },
     },
     {
@@ -23,7 +25,8 @@ const Workouts = [
         locationState: {
             videoId: 'zu1ALQbKU18',
             title: 'HRX',
-            description: "HRX Workout is based on a strength training module.\nIt is designed keeping in mind all age groups and involves working on specific\nmuscles using weights and various movements.\nTry this 30 min HRX Workout with Cult Fit to start burning some fat!"
+            description: "HRX Workout is based on a strength training module.\nIt is designed keeping in mind all age groups and involves working on specific\nmuscles using weights and various movements.\nTry this 30 min HRX Workout with Cult Fit to start burning some fat!",
+            calories: 840
         },
     },
     {
@@ -32,7 +35,8 @@ const Workouts = [
         locationState: {
             videoId: 'yRCUfumiqhk',
             title: 'Power yoga',
-            description: "Yoga is a vast collection of spiritual techniques and practices aimed at integrating mind, body, and spirit to achieve a state of enlightenment or oneness with the universe. What is normally thought of as “yoga” in the West is really Hatha Yoga, one of the many paths of yoga. The different paths of yoga emphasize different approaches and techniques but ultimately lead to the same goal of unification and enlightenment. Hatha Yoga attains the union of mind-body-spirit through a practice of asanas (yoga postures), pranayama (yoga breathing), mudra (body gestures), and shatkarma (internal cleansing). These physical practices are used to purify the body and cultivate prana (life-force energy). Modern Hatha Yoga does not emphasize many of these esoteric practices and instead focuses more on the physical yoga postures. Regardless of what your goals or intentions are for starting, just the yoga poses themselves is a fantastic form of mental and physical exercise."
+            description: "Yoga is a vast collection of spiritual techniques and practices aimed at integrating mind, body, and spirit to achieve a state of enlightenment or oneness with the universe. What is normally thought of as “yoga” in the West is really Hatha Yoga, one of the many paths of yoga. The different paths of yoga emphasize different approaches and techniques but ultimately lead to the same goal of unification and enlightenment. Hatha Yoga attains the union of mind-body-spirit through a practice of asanas (yoga postures), pranayama (yoga breathing), mudra (body gestures), and shatkarma (internal cleansing). These physical practices are used to purify the body and cultivate prana (life-force energy). Modern Hatha Yoga does not emphasize many of these esoteric practices and instead focuses more on the physical yoga postures. Regardless of what your goals or intentions are for starting, just the yoga poses themselves is a fantastic form of mental and physical exercise.",
+            calories: 780
         },
     }
 ];
